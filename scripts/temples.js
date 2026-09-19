a062b496d7515b7c7732534e58993c435cc18b76
olwethumpushe100 <olwethumpushe1@gmail.com>
2026-09-20 00:04:37 +0200

    Fixing temples project

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,11 +7,3 @@ hamburger.addEventListener("click", () => {
   hamburger.setAttribute("aria-expanded", isOpen);
   hamburger.textContent = isOpen ? "✕" : "☰";
 });
-
-// Display the current year in the footer copyright line
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-
-// Display the date the document was last modified
-document.getElementById("lastModified").textContent =
-  `Last Modified: ${document.lastModified}`;
